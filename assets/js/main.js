5e8a15aff3b8f3d7769eed4f807f19fdc478b13c
Deeghayu Adhikari <63475473+DeegayuA@users.noreply.github.com>
2023-02-18 16:33:25 +0530

    nn

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,14 @@
-
-
 // loader
 var myVar;
 
 function myFunction() {
   myVar = setTimeout(showPage, 1000);
-};
+}
 
 function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("main").style.display = "block";
-};
+}
 
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
@@ -23,7 +21,7 @@ if(navToggle){
     navToggle.addEventListener('click', () =>{
         navMenu.classList.add('show-menu')
     })
-};
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
@@ -31,7 +29,7 @@ if(navClose){
     navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
     })
-};
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
@@ -40,7 +38,7 @@ function linkAction(){
     const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
-};
+}
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
@@ -48,12 +46,52 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 function scrollHeader() {
   const header = document.getElementById('header')
   if (this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header'); S
-};
+}
 window.addEventListener('scroll', scrollHeader)
 
 /*===== SHOW SCROLL TOP =====*/
 function scrollTop() {
   const scrollTop = document.getElementById('scroll-top')
   if (this.scrollY >= 500) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll');
-};
+}
 window.addEventListener('scroll', scrollTop)
+/*===== Cursor animation =====*/
+const blob = document.getElementById("blob");
+
+document.body.onpointermove = event => {
+    const { pageX, pageY } = event;
+
+    blob.animate({
+        left:  `${pageX}px`,
+        top: `${pageY}px`
+    }, { duration: 3000, fill: "forwards"});
+};
+/*===== Glitch animation =====*/
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.querySelector('h1','h3').onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 30);
+};
